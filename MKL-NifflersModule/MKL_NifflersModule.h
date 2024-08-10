7b2a0b48c1dcf38054efda0fd3976ca2d486af0e
Nicolas Molina <nicolasjmolina1@gmail.com>
2024-08-10 15:55:36 -0300

    First step: podfile

---
+++ MKL-NifflersModule/MKL_NifflersModule.h
@@ -0,0 +1,18 @@
+//
+//  MKL_NifflersModule.h
+//  MKL-NifflersModule
+//
+//  Created by Nico Molina on 10/08/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MKL_NifflersModule.
+FOUNDATION_EXPORT double MKL_NifflersModuleVersionNumber;
+
+//! Project version string for MKL_NifflersModule.
+FOUNDATION_EXPORT const unsigned char MKL_NifflersModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MKL_NifflersModule/PublicHeader.h>
+
+
